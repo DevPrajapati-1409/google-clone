5e14430c0cf71200ee75fc9243d985607555bb80
DevPrajapati-1409 <devprajapati1409@gmail.com>
2021-09-14 08:25:20 +0530

    Added env variables

--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ module.exports = {
     domains: ["www.google.co.uk"],
   },
   reactStrictMode: true,
+  env: {
+    API_KEY: "AIzaSyAH0OCY_WHYNylWax7m6SoFomTIQACo4mc",
+    CONTEXT_KEY: "f923e1c6252861941",
+  },
 };
